56f48989bca120f94c848911fda5374178069070
LeaFrock <petergeorge@126.com>
2018-09-19 23:51:31 +0800

    增加ProxyAgent类，负责数据层的连接和数据的分配

---
+++ SpiderX.Proxy/ProxyAgent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SpiderX.Proxy
+{
+    public static class ProxyAgent
+    {
+        public static List<SpiderProxyEntity> SelectProxyEntities()
+        {
+            using (var context = new ProxyDbContext())
+            {
+                return context.ProxyEntities.ToList();
+            }
+        }
+
+        public static int AddProxyEntities(IEnumerable<SpiderProxyEntity> entities)
+        {
+            using (var context = new ProxyDbContext())
+            {
+                context.ProxyEntities.AddRange(entities);
+                return context.SaveChanges();
+            }
+        }
+    }
+}
