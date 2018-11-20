be529851d18a9b6352b91861aae2fe882551bf23
LeaFrock <petergeorge@126.com>
2018-11-21 00:39:55 +0800

    Add the decompress functions of HttpResponseMessage

--- SpiderX.Extensions/Http/HttpExtension.cs
+++ SpiderX.Extensions/Http/HttpExtension.cs
@@ -2,60 +2,109 @@
 using System.IO;
 using System.IO.Compression;
 using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SpiderX.Extensions.Http
 {
-	public static class HttpExtension
-	{
-		public static string ToText(this HttpWebResponse response)
-		{
-			Stream responseStream = response.GetResponseStream();
-			//Check Encoding
-			Encoding encoding;
-			if (response.CharacterSet != null)
-			{
-				try
-				{
-					encoding = Encoding.GetEncoding(response.CharacterSet);
-				}
-				catch (ArgumentException)
-				{
-					encoding = Encoding.UTF8;
-					Console.WriteLine("Invalid Encoding: " + response.CharacterSet);
-				}
-			}
-			else
-			{
-				encoding = Encoding.UTF8;
-			}
-			//Check GZip or Deflate
-			Stream readStream;
-			if (response.ContentEncoding != null)
-			{
-				string contentEncoding = response.ContentEncoding.ToLower();
-				if (contentEncoding.Contains("gzip"))
-				{
-					readStream = new GZipStream(responseStream, CompressionMode.Decompress);
-				}
-				else if (contentEncoding.Contains("deflate"))
-				{
-					readStream = new DeflateStream(responseStream, CompressionMode.Decompress);
-				}
-				else
-				{
-					readStream = responseStream;
-				}
-			}
-			else
-			{
-				readStream = responseStream;
-			}
-			//Output Text
-			using (StreamReader sr = new StreamReader(readStream, encoding))
-			{
-				return sr.ReadToEnd();
-			}
-		}
-	}
+    public static class HttpExtension
+    {
+        public static string ToText(this HttpWebResponse response)
+        {
+            Stream responseStream = response.GetResponseStream();
+            //Check Encoding
+            Encoding encoding;
+            if (response.CharacterSet != null)
+            {
+                try
+                {
+                    encoding = Encoding.GetEncoding(response.CharacterSet);
+                }
+                catch (ArgumentException)
+                {
+                    encoding = Encoding.UTF8;
+                    Console.WriteLine("Invalid Encoding: " + response.CharacterSet);
+                }
+            }
+            else
+            {
+                encoding = Encoding.UTF8;
+            }
+            //Check GZip or Deflate
+            Stream readStream;
+            if (response.ContentEncoding != null)
+            {
+                string contentEncoding = response.ContentEncoding.ToLower();
+                if (contentEncoding.Contains("gzip"))
+                {
+                    readStream = new GZipStream(responseStream, CompressionMode.Decompress);
+                }
+                else if (contentEncoding.Contains("deflate"))
+                {
+                    readStream = new DeflateStream(responseStream, CompressionMode.Decompress);
+                }
+                else
+                {
+                    readStream = responseStream;
+                }
+            }
+            else
+            {
+                readStream = responseStream;
+            }
+            //Output Text
+            using (StreamReader sr = new StreamReader(readStream, encoding))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        public async static Task<string> ToTextAsync(this HttpResponseMessage responseMessage)
+        {
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            var responseHeaders = responseMessage.Content.Headers;
+            var contentType = responseHeaders.ContentType;
+            string charset = contentType.CharSet;
+            Encoding encoding;
+            try
+            {
+                encoding = Encoding.GetEncoding(charset);
+            }
+            catch
+            {
+                encoding = Encoding.UTF8;
+            }
+            var contentEncoding = responseHeaders.ContentEncoding;
+            if (contentEncoding.Count > 0)
+            {
+                Stream responseStream = await responseMessage.Content.ReadAsStreamAsync();
+                Stream readStream = null;
+                foreach (string s in contentEncoding)
+                {
+                    if (s.Equals("gzip", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        readStream = new GZipStream(responseStream, CompressionMode.Decompress);
+                        break;
+                    }
+                    if (s.Equals("deflate", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        readStream = new DeflateStream(responseStream, CompressionMode.Decompress);
+                        break;
+                    }
+                }
+                if (readStream != null)
+                {
+                    using (StreamReader sr = new StreamReader(readStream))
+                    {
+                        return await sr.ReadToEndAsync();
+                    }
+                }
+            }
+            return await responseMessage.Content.ReadAsStringAsync();
+        }
+    }
 }
--- SpiderX.Proxy/ProxyAgent.cs
+++ SpiderX.Proxy/ProxyAgent.cs
@@ -5,106 +5,106 @@ using SpiderX.DataClient;
 
 namespace SpiderX.Proxy
 {
-	public sealed class ProxyAgent
-	{
-		public ProxyAgent(DbConfig conf)
-		{
-			DbConfig = conf;
-		}
+    public sealed class ProxyAgent
+    {
+        public ProxyAgent(DbConfig conf)
+        {
+            DbConfig = conf;
+        }
 
-		public DbConfig DbConfig { get; }
+        public DbConfig DbConfig { get; }
 
-		public List<SpiderProxyEntity> SelectTotalProxyEntities()
-		{
-			using (var context = new ProxyDbContext(DbConfig))
-			{
-				return context.ProxyEntities.ToList();
-			}
-		}
+        public List<SpiderProxyEntity> SelectTotalProxyEntities()
+        {
+            using (var context = new ProxyDbContext(DbConfig))
+            {
+                return context.ProxyEntities.ToList();
+            }
+        }
 
-		public List<SpiderProxyEntity> SelectProxyEntities(Func<SpiderProxyEntity, bool> match)
-		{
-			using (var context = new ProxyDbContext(DbConfig))
-			{
-				return context.ProxyEntities.Where(match).ToList();
-			}
-		}
+        public List<SpiderProxyEntity> SelectProxyEntities(Func<SpiderProxyEntity, bool> match)
+        {
+            using (var context = new ProxyDbContext(DbConfig))
+            {
+                return context.ProxyEntities.Where(match).ToList();
+            }
+        }
 
-		public int InsertProxyEntities(IEnumerable<SpiderProxyEntity> entities)
-		{
-			int count = 0;
-			var distinctEntities = entities.Distinct(SpiderProxyEntityComparer.Default);
-			using (var context = new ProxyDbContext(DbConfig))
-			{
-				foreach (var entity in distinctEntities)
-				{
-					if (!context.ProxyEntities.Any(p => p.Host == entity.Host && p.Port == entity.Port))
-					{
-						context.ProxyEntities.Add(entity);
-						count++;
-					}
-				}
-				return count > 0 ? context.SaveChanges() : 0;
-			}
-		}
+        public int InsertProxyEntities(IEnumerable<SpiderProxyEntity> entities)
+        {
+            int count = 0;
+            var distinctEntities = entities.Distinct(SpiderProxyEntityComparer.Default);
+            using (var context = new ProxyDbContext(DbConfig))
+            {
+                foreach (var entity in distinctEntities)
+                {
+                    if (!context.ProxyEntities.Any(p => p.Host == entity.Host && p.Port == entity.Port))
+                    {
+                        context.ProxyEntities.Add(entity);
+                        count++;
+                    }
+                }
+                return count > 0 ? context.SaveChanges() : 0;
+            }
+        }
 
-		public int UpdateProxyEntity(int id, Action<SpiderProxyEntity> update)
-		{
-			using (var context = new ProxyDbContext(DbConfig))
-			{
-				var entity = context.ProxyEntities.Find(id);
-				if (entity != null)
-				{
-					update(entity);
-					return context.SaveChanges();
-				}
-			}
-			return 0;
-		}
+        public int UpdateProxyEntity(int id, Action<SpiderProxyEntity> update)
+        {
+            using (var context = new ProxyDbContext(DbConfig))
+            {
+                var entity = context.ProxyEntities.Find(id);
+                if (entity != null)
+                {
+                    update(entity);
+                    return context.SaveChanges();
+                }
+            }
+            return 0;
+        }
 
-		public int UpdateProxyEntities(IEnumerable<int> ids, Action<SpiderProxyEntity> update)
-		{
-			var distinctIds = ids.Distinct();
-			using (var context = new ProxyDbContext(DbConfig))
-			{
-				foreach (var id in distinctIds)
-				{
-					var entity = context.ProxyEntities.Find(id);
-					if (entity != null)
-					{
-						update(entity);
-					}
-				}
-				return context.SaveChanges();
-			}
-		}
+        public int UpdateProxyEntities(IEnumerable<int> ids, Action<SpiderProxyEntity> update)
+        {
+            var distinctIds = ids.Distinct();
+            using (var context = new ProxyDbContext(DbConfig))
+            {
+                foreach (var id in distinctIds)
+                {
+                    var entity = context.ProxyEntities.Find(id);
+                    if (entity != null)
+                    {
+                        update(entity);
+                    }
+                }
+                return context.SaveChanges();
+            }
+        }
 
-		public int DeleteProxyEntity(string host, int port)
-		{
-			using (var context = new ProxyDbContext(DbConfig))
-			{
-				var entity = context.ProxyEntities.FirstOrDefault(p => p.Host == host && p.Port == port);
-				if (entity != null)
-				{
-					context.ProxyEntities.Remove(entity);
-					return context.SaveChanges();
-				}
-			}
-			return 0;
-		}
+        public int DeleteProxyEntity(string host, int port)
+        {
+            using (var context = new ProxyDbContext(DbConfig))
+            {
+                var entity = context.ProxyEntities.FirstOrDefault(p => p.Host == host && p.Port == port);
+                if (entity != null)
+                {
+                    context.ProxyEntities.Remove(entity);
+                    return context.SaveChanges();
+                }
+            }
+            return 0;
+        }
 
-		public int DeleteProxyEntity(int id)
-		{
-			using (var context = new ProxyDbContext(DbConfig))
-			{
-				var entity = context.ProxyEntities.Find(id);
-				if (entity != null)
-				{
-					context.ProxyEntities.Remove(entity);
-					return context.SaveChanges();
-				}
-			}
-			return 0;
-		}
-	}
+        public int DeleteProxyEntity(int id)
+        {
+            using (var context = new ProxyDbContext(DbConfig))
+            {
+                var entity = context.ProxyEntities.Find(id);
+                if (entity != null)
+                {
+                    context.ProxyEntities.Remove(entity);
+                    return context.SaveChanges();
+                }
+            }
+            return 0;
+        }
+    }
 }
