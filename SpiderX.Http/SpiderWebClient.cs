00f89540f6963e430fe634e364f7b4211baed5e7
LeaFrock <petergeorge@126.com>
2018-11-21 12:52:05 +0800

    Fix some bugs of KuaiDailiProxyBll

--- SpiderX.Http/SpiderWebClient.cs
+++ SpiderX.Http/SpiderWebClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using SpiderX.Extensions.Http;
 
 namespace SpiderX.Http
 {
@@ -26,6 +28,48 @@ namespace SpiderX.Http
 			client.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue() { NoCache = true };
 			client.DefaultRequestHeaders.Pragma.Add(new NameValueHeaderValue("no-cache"));
 			return client;
+		}
+
+		public async Task<string> GetResponse(HttpRequestMessage requestMessage, int retryTimes, Predicate<string> passFunc, int interval = 0)
+		{
+			string result = null;
+			if (passFunc == null)
+			{
+				for (int i = 0; i < retryTimes; i++)
+				{
+					var rMsg = await SendAsync(requestMessage);
+					if (rMsg == null || !rMsg.IsSuccessStatusCode)
+					{
+						continue;
+					}
+					string tempText = await rMsg.ToTextAsync();
+					if (string.IsNullOrEmpty(tempText))
+					{
+						continue;
+					}
+					result = tempText;
+					break;
+				}
+			}
+			else
+			{
+				for (int i = 0; i < retryTimes; i++)
+				{
+					var rMsg = await SendAsync(requestMessage);
+					if (rMsg == null || !rMsg.IsSuccessStatusCode)
+					{
+						continue;
+					}
+					string tempText = await rMsg.ToTextAsync();
+					if (string.IsNullOrEmpty(tempText) || !passFunc(tempText))
+					{
+						continue;
+					}
+					result = tempText;
+					break;
+				}
+			}
+			return result;
 		}
 	}
 }
--- SpiderX.ProxyFetcher/KuaiDaili/KuaiDailiProxyBll.cs
+++ SpiderX.ProxyFetcher/KuaiDaili/KuaiDailiProxyBll.cs
@@ -10,116 +10,115 @@ using SpiderX.Proxy;
 
 namespace SpiderX.ProxyFetcher
 {
-    public sealed class KuaiDailiProxyBll : ProxyBll
-    {
-        internal override ProxyApiProvider ApiProvider { get; } = new KuaiDailiProxyApiProvider();
+	public sealed class KuaiDailiProxyBll : ProxyBll
+	{
+		internal override ProxyApiProvider ApiProvider { get; } = new KuaiDailiProxyApiProvider();
 
-        public const string InhaUrlTemplate = "https://www.kuaidaili.com/free/inha/";//High Anonimity in China
+		public const string InhaUrlTemplate = "https://www.kuaidaili.com/free/inha/";//High Anonimity in China
 
-        public override void Run(params string[] args)
-        {
-            Run();
-        }
+		public override void Run(params string[] args)
+		{
+			Run();
+		}
 
-        public override void Run()
-        {
-            base.Run();
-            ProxyAgent pa = CreateProxyAgent();
-            using (SpiderWebClient webClient = CreateWebClient())
-            {
-                var entities = GetProxyEntities(webClient, InhaUrlTemplate, 10);
-                int insertCount = pa.InsertProxyEntities(entities);
-                Console.WriteLine(insertCount);
-            }
-        }
+		public override void Run()
+		{
+			base.Run();
+			ProxyAgent pa = CreateProxyAgent();
+			using (SpiderWebClient webClient = CreateWebClient())
+			{
+				var entities = GetProxyEntities(webClient, InhaUrlTemplate, 10);
+				int insertCount = pa.InsertProxyEntities(entities);
+				Console.WriteLine(insertCount);
+			}
+		}
 
-        private List<SpiderProxyEntity> GetProxyEntities(SpiderWebClient webClient, string urlTemplate, int maxPage)
-        {
-            List<SpiderProxyEntity> entities = new List<SpiderProxyEntity>(maxPage * 15);
-            var deserilizeTasks = new List<Task>(maxPage);
-            for (int p = 1; p <= maxPage; p++)
-            {
-                string url = GetRequestUrl(urlTemplate, p);
-                string referer = GetRefererUrl(urlTemplate, p);
-                var responseTask = GetResponseMessageAsync(webClient, url, referer);
-                responseTask?.ContinueWith(responseMsg =>
-                    {
-                        HttpResponseMessage responseMessage = responseMsg.Result;
-                        if (responseMessage == null)
-                        {
-                            return;
-                        }
-                        using (responseMessage)
-                        {
-                            if (!responseMessage.IsSuccessStatusCode)
-                            {
-                                return;
-                            }
-                            Task<string> readTask = responseMessage.ToTextAsync();
-                            Task deserilizeTask = readTask.ContinueWith(responseText =>
-                            {
-                                string text = responseText.Result;
-                                var tempList = ApiProvider.GetProxyEntities(text);
-                                if (!tempList.IsNullOrEmpty())
-                                {
-                                    lock (entities)
-                                    {
-                                        entities.AddRange(tempList);
-                                    }
-                                }
-                            });
-                            deserilizeTasks.Add(deserilizeTask);
-                        }
-                    });
-                Thread.Sleep(RandomEvent.Next(4000, 6000));
-            }
-            Task t = Task.WhenAll(deserilizeTasks);
-            try
-            {
-                t.Wait();
-            }
-            catch
-            { }
-            return entities;
-        }
+		private List<SpiderProxyEntity> GetProxyEntities(SpiderWebClient webClient, string urlTemplate, int maxPage)
+		{
+			List<SpiderProxyEntity> entities = new List<SpiderProxyEntity>(maxPage * 15);
+			Task[] tasks = new Task[maxPage];
+			for (int p = 1; p <= maxPage; p++)
+			{
+				string url = GetRequestUrl(urlTemplate, p);
+				string referer = GetRefererUrl(urlTemplate, p);
+				tasks[p - 1] = GetResponseMessageAsync(webClient, url, referer)
+					.ContinueWith(responseMsg =>
+					{
+						HttpResponseMessage responseMessage = responseMsg.Result;
+						if (responseMessage == null)
+						{
+							return;
+						}
+						using (responseMessage)
+						{
+							if (!responseMessage.IsSuccessStatusCode)
+							{
+								return;
+							}
+							Task readTask = responseMessage.ToTextAsync()
+							.ContinueWith(t =>
+							{
+								string text = t.Result;
+								var tempList = ApiProvider.GetProxyEntities(text);
+								if (!tempList.IsNullOrEmpty())
+								{
+									lock (entities)
+									{
+										entities.AddRange(tempList);
+									}
+								}
+							});
+						}
+					});
+				Thread.Sleep(RandomEvent.Next(4000, 6000));
+			}
+			try
+			{
+				Task.WaitAll(tasks);
+			}
+			catch
+			{
+			}
+			return entities;
+		}
 
-        private async Task<HttpResponseMessage> GetResponseMessageAsync(SpiderWebClient webClient, string requestUrl, string referer)
-        {
-            var request = CreateRequestMessage(requestUrl, referer);
-            try
-            {
-                return await webClient.SendAsync(request);
-            }
-            catch (Exception)
-            {
-                request.Dispose();
-                return null;
-            }
-        }
+		private async Task<HttpResponseMessage> GetResponseMessageAsync(SpiderWebClient webClient, string requestUrl, string referer)
+		{
+			var request = CreateRequestMessage(requestUrl, referer);
+			try
+			{
+				return await webClient.SendAsync(request);
+			}
+			catch (Exception)
+			{
+				request.Dispose();
+				return null;
+			}
+		}
 
-        private HttpRequestMessage CreateRequestMessage(string requestUrl, string referer)
-        {
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
-            request.Headers.Referrer = new Uri(referer);
-            return request;
-        }
+		private HttpRequestMessage CreateRequestMessage(string requestUrl, string referer)
+		{
+			HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+			request.Headers.Referrer = new Uri(referer);
+			return request;
+		}
 
-        private SpiderWebClient CreateWebClient()
-        {
-            SpiderWebClient client = SpiderWebClient.CreateDefault();
-            client.InnerClientHandler.UseProxy = false;
-            client.DefaultRequestHeaders.Host = ApiProvider.HomePageHost;
-            client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
-            client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
-            client.DefaultRequestHeaders.Add("Accept-Language", "zh-CN,zh;q=0.9");
-            client.DefaultRequestHeaders.Add("Accept-Charset", "utf-8");
-            client.DefaultRequestHeaders.Add("DNT", "1");
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36");
-            return client;
-        }
+		private SpiderWebClient CreateWebClient()
+		{
+			SpiderWebClient client = SpiderWebClient.CreateDefault();
+			client.InnerClientHandler.UseProxy = false;
+			client.DefaultRequestHeaders.Host = ApiProvider.HomePageHost;
+			client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+			client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+			client.DefaultRequestHeaders.Add("Accept-Language", "zh-CN,zh;q=0.9");
+			client.DefaultRequestHeaders.Add("Accept-Charset", "utf-8");
+			client.DefaultRequestHeaders.Add("DNT", "1");
+			client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36");
+			return client;
+		}
 
-        private static string GetRequestUrl(string urlTemplate, int page) => urlTemplate + page.ToString();
+		private static string GetRequestUrl(string urlTemplate, int page) => urlTemplate + page.ToString();
 
-        private static string GetRefererUrl(string urlTemplate, int page) => urlTemplate + Math.Max(1, page - RandomEvent.Next(1, 5)).ToString();
-    }
+		private static string GetRefererUrl(string urlTemplate, int page) => urlTemplate + Math.Max(1, page - RandomEvent.Next(1, 5)).ToString();
+	}
 }
