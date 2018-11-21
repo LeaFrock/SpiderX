a0b37f001b64e8b9127ec4af91c6f2e0eba18a97
LeaFrock <petergeorge@126.com>
2018-11-21 17:57:15 +0800

    Add new Extensions about HttpClient.

--- SpiderX.Extensions/Http/HttpExtension.cs
+++ SpiderX.Extensions/Http/HttpExtension.cs
@@ -8,103 +8,120 @@ using System.Threading.Tasks;
 
 namespace SpiderX.Extensions.Http
 {
-    public static class HttpExtension
-    {
-        public static string ToText(this HttpWebResponse response)
-        {
-            Stream responseStream = response.GetResponseStream();
-            //Check Encoding
-            Encoding encoding;
-            if (response.CharacterSet != null)
-            {
-                try
-                {
-                    encoding = Encoding.GetEncoding(response.CharacterSet);
-                }
-                catch (ArgumentException)
-                {
-                    encoding = Encoding.UTF8;
-                    Console.WriteLine("Invalid Encoding: " + response.CharacterSet);
-                }
-            }
-            else
-            {
-                encoding = Encoding.UTF8;
-            }
-            //Check GZip or Deflate
-            Stream readStream;
-            if (response.ContentEncoding != null)
-            {
-                string contentEncoding = response.ContentEncoding.ToLower();
-                if (contentEncoding.Contains("gzip"))
-                {
-                    readStream = new GZipStream(responseStream, CompressionMode.Decompress);
-                }
-                else if (contentEncoding.Contains("deflate"))
-                {
-                    readStream = new DeflateStream(responseStream, CompressionMode.Decompress);
-                }
-                else
-                {
-                    readStream = responseStream;
-                }
-            }
-            else
-            {
-                readStream = responseStream;
-            }
-            //Output Text
-            using (StreamReader sr = new StreamReader(readStream, encoding))
-            {
-                return sr.ReadToEnd();
-            }
-        }
+	public static class HttpExtension
+	{
+		public static string ToText(this HttpWebResponse response)
+		{
+			Stream responseStream = response.GetResponseStream();
+			//Check Encoding
+			Encoding encoding;
+			if (response.CharacterSet != null)
+			{
+				try
+				{
+					encoding = Encoding.GetEncoding(response.CharacterSet);
+				}
+				catch (ArgumentException)
+				{
+					encoding = Encoding.UTF8;
+					Console.WriteLine("Invalid Encoding: " + response.CharacterSet);
+				}
+			}
+			else
+			{
+				encoding = Encoding.UTF8;
+			}
+			//Check GZip or Deflate
+			Stream readStream;
+			if (response.ContentEncoding != null)
+			{
+				string contentEncoding = response.ContentEncoding.ToLower();
+				if (contentEncoding.Contains("gzip"))
+				{
+					readStream = new GZipStream(responseStream, CompressionMode.Decompress);
+				}
+				else if (contentEncoding.Contains("deflate"))
+				{
+					readStream = new DeflateStream(responseStream, CompressionMode.Decompress);
+				}
+				else
+				{
+					readStream = responseStream;
+				}
+			}
+			else
+			{
+				readStream = responseStream;
+			}
+			//Output Text
+			using (StreamReader sr = new StreamReader(readStream, encoding))
+			{
+				return sr.ReadToEnd();
+			}
+		}
 
-        public async static Task<string> ToTextAsync(this HttpResponseMessage responseMessage)
-        {
-            if (!responseMessage.IsSuccessStatusCode)
-            {
-                return null;
-            }
-            var responseHeaders = responseMessage.Content.Headers;
-            var contentType = responseHeaders.ContentType;
-            string charset = contentType.CharSet;
-            Encoding encoding;
-            try
-            {
-                encoding = Encoding.GetEncoding(charset);
-            }
-            catch
-            {
-                encoding = Encoding.UTF8;
-            }
-            var contentEncoding = responseHeaders.ContentEncoding;
-            if (contentEncoding.Count > 0)
-            {
-                Stream responseStream = await responseMessage.Content.ReadAsStreamAsync();
-                Stream readStream = null;
-                foreach (string s in contentEncoding)
-                {
-                    if (s.Equals("gzip", StringComparison.CurrentCultureIgnoreCase))
-                    {
-                        readStream = new GZipStream(responseStream, CompressionMode.Decompress);
-                        break;
-                    }
-                    if (s.Equals("deflate", StringComparison.CurrentCultureIgnoreCase))
-                    {
-                        readStream = new DeflateStream(responseStream, CompressionMode.Decompress);
-                        break;
-                    }
-                }
-                if (readStream != null)
-                {
-                    using (StreamReader sr = new StreamReader(readStream))
-                    {
-                        return await sr.ReadToEndAsync();
-                    }
-                }
-            }
-            return await responseMessage.Content.ReadAsStringAsync();
-        }
-    }
+		public async static Task<string> ToTextAsync(this HttpResponseMessage responseMessage)
+		{
+			if (!responseMessage.IsSuccessStatusCode)
+			{
+				return string.Empty;
+			}
+			var content = responseMessage.Content;
+			if (content.Headers.ContentEncoding.IsNullOrEmpty())//ReadAsString directly.
+			{
+				return await responseMessage.Content.ReadAsStringAsync();
+			}
+			Stream finalStream = await content.ToStreamAsync();
+			Encoding encoding = GetEncodingByCharset(content.Headers.ContentType.CharSet);
+			using (finalStream)
+			{
+				using (StreamReader sr = new StreamReader(finalStream, encoding))
+				{
+					return await sr.ReadToEndAsync();
+				}
+			}
+		}
+
+		public async static Task<Stream> ToStreamAsync(this HttpResponseMessage responseMessage)
+		{
+			if (!responseMessage.IsSuccessStatusCode)
+			{
+				return Stream.Null;
+			}
+			return await responseMessage.Content.ToStreamAsync();
+		}
+
+		public async static Task<Stream> ToStreamAsync(this HttpContent content)
+		{
+			Stream source = await content.ReadAsStreamAsync();
+			foreach (string s in content.Headers.ContentEncoding)
+			{
+				if (s.Equals("gzip", StringComparison.CurrentCultureIgnoreCase))
+				{
+					return new GZipStream(source, CompressionMode.Decompress);
+				}
+				if (s.Equals("deflate", StringComparison.CurrentCultureIgnoreCase))
+				{
+					return new DeflateStream(source, CompressionMode.Decompress);
+				}
+			}
+			return source;
+		}
+
+		private static Encoding GetEncodingByCharset(string charset)
+		{
+			if (!string.IsNullOrEmpty(charset))
+			{
+				try
+				{
+					return Encoding.GetEncoding(charset);
+				}
+				catch
+				{
+					return Encoding.UTF8;
+				}
+			}
+			return Encoding.UTF8;
+		}
+	}
 }
--- SpiderX.Http/HttpConsole.cs
+++ SpiderX.Http/HttpConsole.cs
@@ -1,11 +1,11 @@
 ﻿namespace SpiderX.Http
 {
-    public static class HttpConsole
-    {
-        public const string DefaultPcUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
-        public const string DefaultMobileUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1";
+	public static class HttpConsole
+	{
+		public const string DefaultPcUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36";
+		public const string DefaultMobileUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1";
 
-        public static HtmlResponser DefaultHtmlResponser { get; } = new HtmlResponser();
-        public static JsonResponser DefaultJsonResponser { get; } = new JsonResponser();
-    }
+		public static HtmlResponser DefaultHtmlResponser { get; } = new HtmlResponser();
+		public static JsonResponser DefaultJsonResponser { get; } = new JsonResponser();
+	}
 }
