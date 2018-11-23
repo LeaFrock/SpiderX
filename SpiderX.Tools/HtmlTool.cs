d72a30dbe0d0fdbfdf32fcddb0c2a5f1c5c0c957
LeaFrock <petergeorge@126.com>
2018-11-24 01:27:26 +0800

    Fix namespace.

--- SpiderX.Tools/HtmlTool.cs
+++ SpiderX.Tools/HtmlTool.cs
@@ -1,7 +1,7 @@
 ﻿using System.IO;
 using HtmlAgilityPack;
 
-namespace SpiderX.Http
+namespace SpiderX.Tools
 {
 	public static class HtmlTool
 	{
--- SpiderX.Tools/JsonTool.cs
+++ SpiderX.Tools/JsonTool.cs
@@ -3,7 +3,7 @@ using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace SpiderX.Http
+namespace SpiderX.Tools
 {
 	public sealed class JsonTool
 	{
