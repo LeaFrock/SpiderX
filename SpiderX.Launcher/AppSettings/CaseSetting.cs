aeb7094c980aecd2e5f8d03c86152795d4b3606e
LeaFrock <petergeorge@126.com>
2018-11-30 00:50:19 +0800

    Preparing for reconstruction of Launcher.

---
+++ SpiderX.Launcher/AppSettings/CaseSetting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SpiderX.Launcher
+{
+	public sealed class CaseSetting : IEquatable<CaseSetting>
+	{
+		public CaseSetting(string name, string[] paramsAry)
+		{
+			Name = name ?? throw new ArgumentNullException();
+			Params = paramsAry;
+		}
+
+		public string Name { get; }
+
+		public string[] Params { get; }
+
+		public bool Equals(CaseSetting other)
+		{
+			if (Name.EndsWith("bll", StringComparison.CurrentCultureIgnoreCase))
+			{
+				return Name.Contains(other.Name, StringComparison.CurrentCultureIgnoreCase);
+			}
+			return other.Name.Contains(Name, StringComparison.CurrentCultureIgnoreCase);
+		}
+	}
+}
