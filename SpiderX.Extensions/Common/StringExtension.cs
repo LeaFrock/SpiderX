af66bbf2ccafe0b5b8eda9d83ac1bfb3f7055bd3
LeaFrock <petergeorge@126.com>
2018-12-02 23:44:46 +0800

    Add new extensions of String.

--- SpiderX.Extensions/Common/StringExtension.cs
+++ SpiderX.Extensions/Common/StringExtension.cs
@@ -46,7 +46,33 @@ namespace SpiderX.Extensions
 			return strs.All(s => text.Contains(s, comparisonType));
 		}
 
-		//It'll be removed after upgrading to '.Net Standard 2.1' which provides 'String.Contains(String, StringComparison)'.
+		public static bool StartsWithAny(this string text, params string[] strs)
+		{
+			return Array.Exists(strs, s => text.StartsWith(s));
+		}
+
+		public static bool StartsWithAny(this string text, StringComparison comparisonType, params string[] strs)
+		{
+			return Array.Exists(strs, s => text.StartsWith(s, comparisonType));
+		}
+
+		public static bool EndsWithAny(this string text, params string[] strs)
+		{
+			return Array.Exists(strs, s => text.EndsWith(s));
+		}
+
+		public static bool EndsWithAny(this string text, StringComparison comparisonType, params string[] strs)
+		{
+			return Array.Exists(strs, s => text.EndsWith(s, comparisonType));
+		}
+
+		/// <summary>
+		/// It'll be removed after upgrading to '.Net Standard 2.1' which provides 'String.Contains(String, StringComparison)'.
+		/// </summary>
+		/// <param name="text">Text</param>
+		/// <param name="value">TargetValue</param>
+		/// <param name="comparisonType">ComparisonType</param>
+		/// <returns></returns>
 		private static bool Contains(this string text, string value, StringComparison comparisonType)
 		{
 			return text.IndexOf(value, comparisonType) >= 0;
