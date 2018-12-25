06ba11148c414effab519787e333084cf1aea034
LeaFrock <petergeorge@126.com>
2018-12-25 21:28:57 +0800

    Add StringComparison Options.

--- SpiderX.Proxy/Validators/SimpleProxyValidator.cs
+++ SpiderX.Proxy/Validators/SimpleProxyValidator.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using SpiderX.Http;
 
@@ -24,9 +25,15 @@ namespace SpiderX.Proxy
 
 		public string Firstword { get; set; }
 
+		public StringComparison FirstwordComparisonType { get; set; }
+
 		public string Keyword { get; set; }
 
+		public StringComparison KeywordComparisonType { get; set; }
+
 		public string Lastword { get; set; }
+
+		public StringComparison LastwordComparisonType { get; set; }
 
 		public override bool CheckPass(IWebProxy proxy)
 		{
@@ -42,7 +49,7 @@ namespace SpiderX.Proxy
 				{
 					try
 					{
-						responseText = webClient.GetStringAsync(TargetUri).Result;
+						responseText = webClient.GetStringAsync(TargetUri).Result?.Trim();
 					}
 					catch
 					{
@@ -52,15 +59,15 @@ namespace SpiderX.Proxy
 					{
 						continue;
 					}
-					if (!string.IsNullOrEmpty(Lastword) && !responseText.EndsWith(Lastword))
+					if (!string.IsNullOrEmpty(Lastword) && !responseText.EndsWith(Lastword, LastwordComparisonType))
 					{
 						continue;
 					}
-					if (!string.IsNullOrEmpty(Firstword) && !responseText.StartsWith(Firstword))
+					if (!string.IsNullOrEmpty(Firstword) && !responseText.StartsWith(Firstword, FirstwordComparisonType))
 					{
 						continue;
 					}
-					if (!string.IsNullOrEmpty(Keyword) && !responseText.Contains(Keyword))
+					if (!string.IsNullOrEmpty(Keyword) && !responseText.Contains(Keyword, KeywordComparisonType))
 					{
 						continue;
 					}
