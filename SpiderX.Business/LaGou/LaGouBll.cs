413e0452737e1b588c4bb94a951b5746e27e509d
LeaFrock <petergeorge@126.com>
2019-01-24 02:06:12 +0800

    Add LaGouBll.

---
+++ SpiderX.Business/LaGou/LaGouBll.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SpiderX.BusinessBase;
+
+namespace SpiderX.Business.LaGou
+{
+	public sealed class LaGouBll : BllBase
+	{
+		public override void Run(params string[] args)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
