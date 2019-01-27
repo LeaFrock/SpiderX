a774f281cc70da37fecb9c0d531e8de30b242f22
LeaFrock <petergeorge@126.com>
2019-01-28 02:05:59 +0800

    Add new ProxyBll.

---
+++ SpiderX.ProxyFetcher/KxDaili/KxDailiProxyApiProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SpiderX.Http;
+
+namespace SpiderX.ProxyFetcher
+{
+	internal sealed class KxDailiProxyApiProvider : ProxyApiProvider
+	{
+		public KxDailiProxyApiProvider() : base()
+		{
+			HomePageHost = "ip.kxdaili.com";
+			HomePageUrl = "http://ip.kxdaili.com/";
+		}
+
+		public const string IpUrl = "http://ip.kxdaili.com/dailiip.html";
+
+		public override SpiderWebClient CreateWebClient()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ SpiderX.ProxyFetcher/KxDaili/KxDailiProxyBll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpiderX.ProxyFetcher
+{
+	internal class KxDailiProxyBll : ProxyBll
+	{
+		internal override ProxyApiProvider ApiProvider { get; } = new KxDailiProxyApiProvider();
+
+		public override void Run(params string[] args)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
