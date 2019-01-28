eade1773e4215bf535c22e2aef78db171cb06ddc
LeaFrock <petergeorge@126.com>
2019-01-28 20:42:25 +0800

    Update StringTool.

--- SpiderX.Business/LaGou/LaGouBll.cs
+++ SpiderX.Business/LaGou/LaGouBll.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using SpiderX.BusinessBase;
+using SpiderX.Tools;
 
 namespace SpiderX.Business.LaGou
 {
 	public sealed class LaGouBll : BllBase
 	{
+		public override void Run()
+		{
+		}
+
 		public override void Run(params string[] args)
 		{
-			throw new NotImplementedException();
+			Run();
 		}
 	}
 }
--- SpiderX.Tools/StringTool.cs
+++ SpiderX.Tools/StringTool.cs
@@ -3,19 +3,51 @@ using System.Text.RegularExpressions;
 
 namespace SpiderX.Tools
 {
-    public static class StringTool
-    {
-        private readonly static Regex _doubleRegex = new Regex(@"\d+(\.\d+)?", RegexOptions.None, TimeSpan.FromMilliseconds(500));
+	public static class StringTool
+	{
+		#region Value
 
-        public static bool TryMatchDouble(string text, out double value)
-        {
-            Match m = _doubleRegex.Match(text);
-            if (!m.Success)
-            {
-                value = 0d;
-                return false;
-            }
-            return double.TryParse(m.Value, out value);
-        }
-    }
+		private readonly static Regex _doubleRegex = new Regex(@"\d+(\.\d+)?", RegexOptions.None, TimeSpan.FromMilliseconds(500));
+
+		public static int MatchInt(string text, int defaultValue = 0)
+		{
+			return TryMatchInt(text, out int result) ? result : defaultValue;
+		}
+
+		public static bool TryMatchInt(string text, out int value)
+		{
+			Match m = _doubleRegex.Match(text);
+			if (!m.Success)
+			{
+				value = 0;
+				return false;
+			}
+			return int.TryParse(m.Value, out value);
+		}
+
+		public static double MatchDouble(string text, double defaultValue = 0)
+		{
+			return TryMatchDouble(text, out double result) ? result : defaultValue;
+		}
+
+		public static bool TryMatchDouble(string text, out double value)
+		{
+			Match m = _doubleRegex.Match(text);
+			if (!m.Success)
+			{
+				value = 0d;
+				return false;
+			}
+			return double.TryParse(m.Value, out value);
+		}
+
+		#endregion Value
+
+		#region IP
+
+		private readonly static Regex _ipv4Regex = new Regex(@"(?=(\b|\D))(((\d{1,2})|(1\d{1,2})|(2[0-4]\d)|(25[0-5]))\.){3}((\d{1,2})|(1\d{1,2})|(2[0-4]\d)|(25[0-5]))(?=(\b|\D))", RegexOptions.None, TimeSpan.FromMilliseconds(500));
+		private readonly static Regex _ipv6Regex = new Regex(@"^([\\da-fA-F]{1,4}:){7}([\\da-fA-F]{1,4})$", RegexOptions.None, TimeSpan.FromMilliseconds(500));
+
+		#endregion IP
+	}
 }
