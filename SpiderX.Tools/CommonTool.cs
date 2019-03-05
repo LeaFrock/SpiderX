b374315ec7382f8f14553358cfe913575cf48281
LeaFrock <petergeorge@126.com>
2019-03-06 01:57:07 +0800

    Add a function for converting between Long and DateTime.

--- SpiderX.Tools/CommonTool.cs
+++ SpiderX.Tools/CommonTool.cs
@@ -5,5 +5,12 @@ namespace SpiderX.Tools
 	public static class CommonTool
 	{
 		public readonly static Random RandomEvent = new Random();
+
+		public readonly static DateTime DbMinTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Local);
+
+		public static DateTime FromTimestampMillisec(long millisec)
+		{
+			return DbMinTime.AddMilliseconds(millisec);
+		}
 	}
 }
