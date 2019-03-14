f792cddf82e792945df600c75ff3a7861eb689d1
LeaFrock <petergeorge@126.com>
2019-03-14 20:13:06 +0800

    Add a new extension function of DateTime.

--- SpiderX.Extensions/DateTime/DateTimeExtension.cs
+++ SpiderX.Extensions/DateTime/DateTimeExtension.cs
@@ -4,19 +4,36 @@ namespace SpiderX.Extensions
 {
 	public static class DateTimeExtension
 	{
+		private const long TicksOfDbBirth = 621355968000000000;
+
 		public static long ToTimestamp(this DateTime dt)
 		{
-			return (dt.Ticks - 621355968000000000) / 10000000;
+			return (dt.Ticks - TicksOfDbBirth) / 10_000_000;
 		}
 
 		public static long ToTimestampMillisec(this DateTime dt)
 		{
-			return (dt.Ticks - 621355968000000000) / 10000;
+			return (dt.Ticks - TicksOfDbBirth) / 10_000;
+		}
+
+		public static int ToShortDateNumber(this DateTime dt)
+		{
+			return dt.Year * 100 + dt.Month;
 		}
 
 		public static int ToDateNumber(this DateTime dt)
 		{
-			return dt.Year * 10000 + dt.Month * 100 + dt.Day;
+			return dt.Year * 10_000 + dt.Month * 100 + dt.Day;
+		}
+
+		public static long ToLongDateNumber(this DateTime dt)
+		{
+			return dt.Year * 10_000_000_000 + dt.Month * 100_000_000 + dt.Day * 1_000_000 + dt.Hour * 10_000 + dt.Minute * 100 + dt.Second;
+		}
+
+		public static long ToLongestDateNumber(this DateTime dt)
+		{
+			return ToLongDateNumber(dt) * 1000 + dt.Millisecond;
 		}
 	}
 }
