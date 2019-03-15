9c14f3f25897b97ac6eedc8abdbbed3323e42572
LeaFrock <petergeorge@126.com>
2019-03-15 19:34:37 +0800

    Move Local WebProxy to SpiderX.Http.HttpConsole

--- SpiderX.Tools/WebTool.cs
+++ SpiderX.Tools/WebTool.cs
@@ -4,11 +4,6 @@ namespace SpiderX.Tools
 {
 	public static class WebTool
 	{
-		/// <summary>
-		/// Use for local web debugging tools, like Fiddler etc.
-		/// </summary>
-		public readonly static IWebProxy Local = new WebProxy("localhost", 8888);
-
 		public static string UrlEncode(string text)
 		{
 			return WebUtility.UrlEncode(text);
