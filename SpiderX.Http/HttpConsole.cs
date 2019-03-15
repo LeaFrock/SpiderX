fe747ab372ea9d03d32146fef6938ae5aa5fdbd3
LeaFrock <petergeorge@126.com>
2019-03-15 19:36:05 +0800

    Cache ever-used ContentTypes.

--- SpiderX.Http/HttpConsole.cs
+++ SpiderX.Http/HttpConsole.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http.Headers;
 using SpiderX.Http.Util;
 using SpiderX.Proxy;
@@ -11,7 +13,37 @@ namespace SpiderX.Http
 		public const string DefaultPcUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36";
 		public const string DefaultMobileUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36";
 
-		public readonly static MediaTypeHeaderValue FormContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded") { CharSet = "UTF-8" };
+		/// <summary>
+		/// Use for local web debugging tools, like Fiddler etc.
+		/// </summary>
+		public readonly static IWebProxy LocalWebProxy = new WebProxy("localhost", 8888);
+
+		private readonly static ConcurrentDictionary<string, MediaTypeHeaderValue> _contentTypeDict = new ConcurrentDictionary<string, MediaTypeHeaderValue>();
+
+		public static IReadOnlyDictionary<string, MediaTypeHeaderValue> ContentTypeDict => _contentTypeDict;
+
+		/// <summary>
+		/// Get the ContentType from caches by "[mediaType];[charSet]"
+		/// </summary>
+		/// <param name="key">The key must be in the format "[mediaType];[charSet]"</param>
+		/// <returns></returns>
+		public static MediaTypeHeaderValue GetContentType(string key)
+		{
+			return _contentTypeDict.GetOrAdd(key, CreateContentTypeByKey);
+		}
+
+		private static MediaTypeHeaderValue CreateContentTypeByKey(string key)
+		{
+			int index = key.IndexOf(';');
+			if (index < 1)
+			{
+				throw new FormatException();
+			}
+			ReadOnlySpan<char> span = key.ToCharArray();
+			var mediaTypeSpan = span.Slice(0, index);
+			var charSetSpan = span.Slice(index + 1);
+			return new MediaTypeHeaderValue(new string(mediaTypeSpan)) { CharSet = new string(charSetSpan) };
+		}
 
 		public static SpiderWebProxy CreateDefaultSpiderWebProxy(IEnumerable<Uri> proxies)
 		{
