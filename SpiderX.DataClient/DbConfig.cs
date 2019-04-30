d22368cc4e7d00dc3f6936c7e24451fa284d9a3d
LeaFrock <petergeorge@126.com>
2019-04-30 15:27:25 +0800

    Code fixes.

--- SpiderX.DataClient/DbConfig.cs
+++ SpiderX.DataClient/DbConfig.cs
@@ -4,48 +4,60 @@ using Microsoft.Extensions.Configuration;
 
 namespace SpiderX.DataClient
 {
-	public sealed class DbConfig
-	{
-		private static int _globalTempId = 0;
+    public sealed class DbConfig
+    {
+        private static int _globalTempId = 0;
 
-		public int TempId { get; private set; }
+        private DbConfig()
+        {
+            TempId = Interlocked.Increment(ref _globalTempId);
+        }
 
-		public string Name { get; private set; }
+        public DbConfig(string name, DbEnum type, string connStr, bool isTest) : this()
+        {
+            Name = name;
+            Type = type;
+            ConnectionString = connStr;
+            IsTest = isTest;
+        }
 
-		public DbEnum Type { get; private set; }
+        public int TempId { get; }
 
-		public string ConnectionString { get; private set; }
+        public string Name { get; private set; }
 
-		public bool IsTest { get; private set; } = true;
+        public DbEnum Type { get; private set; }
 
-		public static DbConfig FromConfiguration(IConfiguration source)
-		{
-			DbConfig instance = new DbConfig();
-			string enabledStr = source.GetSection("IsEnabled").Value;
-			if (!bool.TryParse(enabledStr, out bool isEnabled) || !isEnabled)
-			{
-				return null;
-			}
-			string connection = source.GetSection(nameof(ConnectionString)).Value?.Trim();
-			if (string.IsNullOrEmpty(connection))
-			{
-				return null;
-			}
-			string typeStr = source.GetSection(nameof(Type)).Value;
-			if (!Enum.TryParse(typeStr, out DbEnum type))
-			{
-				return null;
-			}
-			instance.Type = type;
-			string testStr = source.GetSection(nameof(IsTest)).Value;
-			if (bool.TryParse(testStr, out bool isTest))
-			{
-				instance.IsTest = isTest;
-			}
-			instance.Name = source.GetSection(nameof(Name)).Value ?? "???";
-			instance.ConnectionString = connection;
-			instance.TempId = Interlocked.Increment(ref _globalTempId);
-			return instance;
-		}
-	}
+        public string ConnectionString { get; private set; }
+
+        public bool IsTest { get; private set; } = true;
+
+        public static DbConfig FromConfiguration(IConfiguration source)
+        {
+            DbConfig instance = new DbConfig();
+            string enabledStr = source.GetSection("IsEnabled").Value;
+            if (!bool.TryParse(enabledStr, out bool isEnabled) || !isEnabled)
+            {
+                return null;
+            }
+            string connection = source.GetSection(nameof(ConnectionString)).Value?.Trim();
+            if (string.IsNullOrEmpty(connection))
+            {
+                return null;
+            }
+            string typeStr = source.GetSection(nameof(Type)).Value;
+            if (!Enum.TryParse(typeStr, out DbEnum type))
+            {
+                return null;
+            }
+            instance.Type = type;
+            string testStr = source.GetSection(nameof(IsTest)).Value;
+            if (bool.TryParse(testStr, out bool isTest))
+            {
+                instance.IsTest = isTest;
+            }
+            instance.Name = source.GetSection(nameof(Name)).Value ?? "???";
+            instance.ConnectionString = connection;
+            return instance;
+        }
+    }
 }
