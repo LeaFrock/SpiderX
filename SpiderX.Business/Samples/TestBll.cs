9a63a92e311d28b8d24441972896d3ddd89258e7
LeaFrock <petergeorge@126.com>
2019-05-17 19:54:04 +0800

    Code fix.

--- SpiderX.Business/Samples/TestBll.cs
+++ SpiderX.Business/Samples/TestBll.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using SpiderX.BusinessBase;
 using SpiderX.DataClient;
+using SpiderX.Http;
 using SpiderX.Http.Util;
 using SpiderX.Proxy;
 
@@ -23,6 +26,36 @@ namespace SpiderX.Business.Samples
 			{
 				throw new DbConfigNotFoundException();
 			}
+			var proxyAgent = ProxyAgent<SqlServerProxyDbContext>.CreateInstance(conf, c => new SqlServerProxyDbContext(c));
+			DefaultProxyUriLoader proxyUriLoader = new DefaultProxyUriLoader()
+			{
+				Days = 360,
+				Condition = p => p.Id > 0,
+				ProxyAgent = proxyAgent
+			};
+			DefaultWebProxySelector proxySelector = new DefaultWebProxySelector(new Uri("http://www.baidu.com"), proxyUriLoader, CreateWebClient, ValidateWebProxy)
+			{
+				UseThresold = 1,
+				VerifyPauseThresold = 2
+			};
+		}
+
+		public static SpiderWebClient CreateWebClient(IWebProxy proxy)
+		{
+			SpiderWebClient client = new SpiderWebClient(proxy);
+			client.DefaultRequestHeaders.Host = "www.baidu.com";
+			client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+			client.DefaultRequestHeaders.Add("Accept-Language", "zh-CN,zh;q=0.9");
+			client.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
+			client.DefaultRequestHeaders.Add("Pragma", "no-cache");
+			client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
+			client.DefaultRequestHeaders.Add("User-Agent", HttpConsole.DefaultPcUserAgent);
+			return client;
+		}
+
+		private static bool ValidateWebProxy(string rspText)
+		{
+			return rspText.EndsWith("</html>") && rspText.Contains("baidu");
 		}
 	}
 }
---
+++ SpiderX.DependencyInjection/SpiderServiceContainer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SpiderX.DependencyInjection
+{
+	public sealed class SpiderServiceContainer
+	{
+		public SpiderServiceContainer() : this(new ServiceCollection())
+		{
+		}
+
+		public SpiderServiceContainer(IServiceCollection services)
+		{
+			Services = services;
+		}
+
+		public IServiceCollection Services { get; }
+	}
+}
