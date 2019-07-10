c6d6a3c7f052294e814a7be93717d06f1da06553
LeaFrock <petergeorge@126.com>
2019-07-10 18:03:49 +0800

    Delete the 'RandomEvent' in CommonTool. Use RandomTool instead.

--- SpiderX.Tools/CommonTool.cs
+++ SpiderX.Tools/CommonTool.cs
@@ -4,8 +4,6 @@ namespace SpiderX.Tools
 {
 	public static class CommonTool
 	{
-		public readonly static Random RandomEvent = new Random();
-
 		public readonly static DateTime DbMinTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Local);
 
 		public static DateTime FromTimestampMillisec(long millisec)
