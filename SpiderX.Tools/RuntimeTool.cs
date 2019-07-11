ab6ff848c51def9e35f4fa490ce6f9225d25e25f
LeaFrock <petergeorge@126.com>
2019-07-11 19:28:41 +0800

    Add RandomTool.

---
+++ SpiderX.Tools/RuntimeTool.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SpiderX.Tools
+{
+	public static class RuntimeTool
+	{
+		internal static (OSPlatform osPlatform, Architecture architecture) GetCurrentPlatform()
+		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				return (OSPlatform.OSX, RuntimeInformation.OSArchitecture);
+			}
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				return (OSPlatform.OSX, RuntimeInformation.OSArchitecture);
+			}
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				return (OSPlatform.OSX, RuntimeInformation.OSArchitecture);
+			}
+			return (OSPlatform.Create("Unknown"), RuntimeInformation.OSArchitecture);
+		}
+	}
+}
