84acf3272f423a30e0d8811fcc0ee08b8ab7cba5
LeaFrock <petergeorge@126.com>
2019-08-01 12:15:04 +0800

    Add BllHelper.

---
+++ SpiderX.Business/InternBllHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using SpiderX.Http;
+
+namespace SpiderX.Business
+{
+	internal static class InternBllHelper
+	{
+		internal static HttpRequestCounter CreateHttpRequestCounter(string name, ILogger logger = null, bool replaceLoggerIfExists = false)
+		{
+			if (!HttpRequestCounter.TryRegisterNew(name, out var counter))
+			{
+				return null;
+			}
+			if (logger != null && (replaceLoggerIfExists || HttpRequestCounter.Logger == null))
+			{
+				HttpRequestCounter.Logger = logger;
+			}
+			return counter;
+		}
+	}
+}
