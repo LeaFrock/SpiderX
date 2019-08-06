7db189728ecf2b208901b97c0bb925fb3547d162
LeaFrock <petergeorge@126.com>
2019-08-06 19:54:06 +0800

    Code fix.

--- SpiderX.Puppeteer/PuppeteerConsole.cs
+++ SpiderX.Puppeteer/PuppeteerConsole.cs
@@ -8,21 +8,26 @@ namespace SpiderX.Puppeteer
 {
 	public static class PuppeteerConsole
 	{
-		private readonly static BrowserFetcher _browserFetcher = new BrowserFetcher(new BrowserFetcherOptions()
-		{
-			Host = Environment.GetEnvironmentVariable("PUPPETEER_CHROMIUM_DOWNLOADHOST")
-		});
+		private readonly static object _downloadSyncRoot = new object();
+
+		private volatile static bool existsChrome;
 
 		private readonly static List<Browser> _browsers = new List<Browser>(2);
 
 		public static IReadOnlyList<Browser> Browsers => _browsers;
 
-		public static async Task<Browser> LauncherBrowser(bool useHeadless, int revision = 669486)
+		public static async Task<Browser> LauncherBrowser(bool useHeadless, string exePath = null, int revision = 0)
 		{
-			string exePath = Environment.GetEnvironmentVariable("PUPPETEER_EXECUTABLE_PATH");
-			if (!File.Exists(exePath))
+			if (!existsChrome)
 			{
-				await _browserFetcher.DownloadAsync(revision);
+				lock (_downloadSyncRoot)
+				{
+					if (!existsChrome)
+					{
+						DownloadAsync(exePath, revision).GetAwaiter().GetResult();
+						existsChrome = true;
+					}
+				}
 			}
 			var browser = await PuppeteerSharp.Puppeteer.LaunchAsync(new LaunchOptions
 			{
@@ -42,30 +47,27 @@ namespace SpiderX.Puppeteer
 			_browsers.Clear();
 		}
 
-		private static async Task<bool> TryDownloadChromeAsync(int revision = 0, string exePath = null)
+		private static async Task DownloadAsync(string exePath, int revision)
 		{
-			if (revision < 1)
+			string path = exePath ?? Environment.GetEnvironmentVariable("PUPPETEER_EXECUTABLE_PATH");
+			if (!File.Exists(path))
 			{
-				string revisionStr = Environment.GetEnvironmentVariable("PUPPETEER_CHROMIUM_REVISION");
-				if (!int.TryParse(revisionStr, out revision))
+				var fetchOpt = new BrowserFetcherOptions()
 				{
-					revision = BrowserFetcher.DefaultRevision;
-				}
-			}
-			if (string.IsNullOrEmpty(exePath))
-			{
-				exePath = Environment.GetEnvironmentVariable("PUPPETEER_EXECUTABLE_PATH");
-				await _browserFetcher.DownloadAsync(revision);
-			}
-			else
-			{
-				if (File.Exists(exePath))
+					Host = Environment.GetEnvironmentVariable("PUPPETEER_CHROMIUM_DOWNLOADHOST"),
+					Path = path
+				};
+				var fetcher = new BrowserFetcher(fetchOpt);
+				if (revision < 1)
 				{
-					return true;
+					string revisionStr = Environment.GetEnvironmentVariable("PUPPETEER_CHROMIUM_REVISION");
+					if (!int.TryParse(revisionStr, out revision) || revision < 1)
+					{
+						revision = BrowserFetcher.DefaultRevision;
+					}
 				}
-				await _browserFetcher.DownloadAsync(revision);
+				await fetcher.DownloadAsync(revision).ConfigureAwait(false);
 			}
-			return false;
 		}
 	}
 }
---
+++ SpiderX.Puppeteer/PuppeteerExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using PuppeteerSharp;
+
+namespace SpiderX.Puppeteer
+{
+	public static class PuppeteerExtension
+	{
+		public static async Task ScrollBy(this Page page, int x, int y)
+		{
+			await page.EvaluateFunctionAsync("_ => {window.scrollBy(" + x.ToString() + "," + y.ToString() + ")}");
+		}
+	}
+}
