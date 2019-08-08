b05ffb32315bd0f46e121806844df8847d2ff637
LeaFrock <petergeorge@126.com>
2019-08-08 19:34:00 +0800

    Add SpiderX.Redis.

---
+++ SpiderX.Redis/SpiderRedisClient.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using StackExchange.Redis;
+
+namespace SpiderX.Redis
+{
+	public sealed class SpiderRedisClient
+	{
+		private SpiderRedisClient()
+		{
+		}
+
+		public ConnectionMultiplexer ConnectionMultiplexer { get; private set; }
+
+		public static SpiderRedisClient Create(ConfigurationOptions configuration, TextWriter log = null)
+		{
+			var client = new SpiderRedisClient();
+			client.ConnectionMultiplexer = ConnectionMultiplexer.Connect(configuration, log);
+			return client;
+		}
+
+		public static SpiderRedisClient Create()
+		{
+			var client = new SpiderRedisClient
+			{
+				ConnectionMultiplexer = ConnectionMultiplexer.Connect("localhost")
+			};
+			return client;
+		}
+	}
+}
