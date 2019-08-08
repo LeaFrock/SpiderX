c07b18170d7fa67937e4786a1ccd8bf1dcb896f4
LeaFrock <petergeorge@126.com>
2019-08-08 16:46:41 +0800

    Try to add the module of Http(s) proxy server.

---
+++ SpiderX.Proxy/Server/SpiderProxyServer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.Models;
+
+namespace SpiderX.Proxy
+{
+	public sealed class SpiderProxyServer : ProxyServer
+	{
+		private SpiderProxyServer()
+		{
+		}
+
+		private int _closeTimes;
+
+		public bool IsClosed => _closeTimes != 0;
+
+		private readonly ConcurrentBag<SpiderProxyServerEventOption> _eventOptions = new ConcurrentBag<SpiderProxyServerEventOption>();
+
+		public IReadOnlyCollection<SpiderProxyServerEventOption> EventOptions => _eventOptions;
+
+		public bool TryAddEventOption(SpiderProxyServerEventOption eventOption)
+		{
+			if (IsClosed)
+			{
+				return false;
+			}
+			if (eventOption is null)
+			{
+				return false;
+			}
+			_eventOptions.Add(eventOption);
+			eventOption.Bind(this);
+			return true;
+		}
+
+		public void Close()
+		{
+			if (IsClosed)
+			{
+				return;
+			}
+			if (Interlocked.CompareExchange(ref _closeTimes, 1, 0) != 0)//Ensure that only one thread can move next.
+			{
+				return;
+			}
+			if (!_eventOptions.IsEmpty)
+			{
+				foreach (var eventOpt in _eventOptions)
+				{
+					eventOpt.UnBind(this);
+				}
+				_eventOptions.Clear();
+			}
+			Dispose();
+		}
+
+		public static SpiderProxyServer StartNew(ExplicitProxyEndPoint endPoint, SpiderProxyServerEventOption eventOption)
+		{
+			var server = new SpiderProxyServer();
+			server.TryAddEventOption(eventOption);
+			server.CertificateManager.TrustRootCertificate(true);//locally trust root certificate used by this proxy.
+			server.AddEndPoint(endPoint);
+			server.SetAsSystemHttpProxy(endPoint);
+			server.SetAsSystemHttpsProxy(endPoint);
+			server.Start();
+			return server;
+		}
+	}
+}
---
+++ SpiderX.Proxy/Server/SpiderProxyServerEventOption.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Net.Sockets;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+
+namespace SpiderX.Proxy
+{
+	public sealed class SpiderProxyServerEventOption
+	{
+		public AsyncEventHandler<SessionEventArgs> AfterResponse { get; set; }
+		public AsyncEventHandler<SessionEventArgs> BeforeResponse { get; set; }
+		public AsyncEventHandler<SessionEventArgs> BeforeRequest { get; set; }
+		public AsyncEventHandler<CertificateSelectionEventArgs> ClientCertificateSelectionCallback { get; set; }
+		public AsyncEventHandler<CertificateValidationEventArgs> ServerCertificateValidationCallback { get; set; }
+		public EventHandler ServerConnectionCountChanged { get; set; }
+		public EventHandler ClientConnectionCountChanged { get; set; }
+		public AsyncEventHandler<TcpClient> OnServerConnectionCreate { get; set; }
+		public AsyncEventHandler<TcpClient> OnClientConnectionCreate { get; set; }
+
+		internal void Bind(ProxyServer server)
+		{
+			if (AfterResponse != null)
+			{
+				server.AfterResponse += AfterResponse;
+			}
+			if (BeforeResponse != null)
+			{
+				server.BeforeResponse += BeforeResponse;
+			}
+			if (BeforeRequest != null)
+			{
+				server.BeforeRequest += BeforeRequest;
+			}
+			if (ClientCertificateSelectionCallback != null)
+			{
+				server.ClientCertificateSelectionCallback += ClientCertificateSelectionCallback;
+			}
+			if (ServerCertificateValidationCallback != null)
+			{
+				server.ServerCertificateValidationCallback += ServerCertificateValidationCallback;
+			}
+			if (ServerConnectionCountChanged != null)
+			{
+				server.ServerConnectionCountChanged += ServerConnectionCountChanged;
+			}
+			if (ClientConnectionCountChanged != null)
+			{
+				server.ClientConnectionCountChanged += ClientConnectionCountChanged;
+			}
+			if (OnServerConnectionCreate != null)
+			{
+				server.OnServerConnectionCreate += OnServerConnectionCreate;
+			}
+			if (OnClientConnectionCreate != null)
+			{
+				server.OnClientConnectionCreate += OnClientConnectionCreate;
+			}
+		}
+
+		internal void UnBind(ProxyServer server)
+		{
+			if (AfterResponse != null)
+			{
+				server.AfterResponse -= AfterResponse;
+			}
+			if (BeforeResponse != null)
+			{
+				server.BeforeResponse -= BeforeResponse;
+			}
+			if (BeforeRequest != null)
+			{
+				server.BeforeRequest -= BeforeRequest;
+			}
+			if (ClientCertificateSelectionCallback != null)
+			{
+				server.ClientCertificateSelectionCallback -= ClientCertificateSelectionCallback;
+			}
+			if (ServerCertificateValidationCallback != null)
+			{
+				server.ServerCertificateValidationCallback -= ServerCertificateValidationCallback;
+			}
+			if (ServerConnectionCountChanged != null)
+			{
+				server.ServerConnectionCountChanged -= ServerConnectionCountChanged;
+			}
+			if (ClientConnectionCountChanged != null)
+			{
+				server.ClientConnectionCountChanged -= ClientConnectionCountChanged;
+			}
+			if (OnServerConnectionCreate != null)
+			{
+				server.OnServerConnectionCreate -= OnServerConnectionCreate;
+			}
+			if (OnClientConnectionCreate != null)
+			{
+				server.OnClientConnectionCreate -= OnClientConnectionCreate;
+			}
+		}
+	}
+}
