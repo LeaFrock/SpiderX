a2abbe8ea6322a4177f9f7024d783063980db953
LeaFrock <petergeorge@126.com>
2019-12-05 17:34:28 +0800

    Preparing for replacing Json.NET with System.Text.Json.

--- SpiderX.Tools/JsonTool.cs
+++ SpiderX.Tools/JsonTool.cs
@@ -1,14 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+
+//using System.Text;
+//using System.Text.Json;
 
 namespace SpiderX.Tools
 {
 	public sealed class JsonTool
 	{
+		//public static T DeserializeObject<T>(string text, bool throwError = false) where T : class
+		//{
+		//	ReadOnlySpan<byte> bytes = Encoding.UTF8.GetBytes(text);
+		//	T result;
+		//	try
+		//	{
+		//		result = JsonSerializer.Deserialize<T>(bytes);
+		//	}
+		//	catch (JsonException ex)
+		//	{
+		//		if (throwError)
+		//		{
+		//			throw ex;
+		//		}
+		//		return null;
+		//	}
+		//	return result;
+		//}
+
 		public static T DeserializeObject<T>(string text, bool throwError = false) where T : class
 		{
 			T result;
@@ -23,54 +41,6 @@ namespace SpiderX.Tools
 					throw ex;
 				}
 				return null;
-			}
-			return result;
-		}
-
-		public static JToken DeserializeObject(Stream stream, Encoding encoding, bool throwError = false)
-		{
-			JsonSerializer ser = JsonSerializer.CreateDefault();
-			using (StreamReader sr = new StreamReader(stream, encoding))
-			{
-				using (JsonTextReader jtr = new JsonTextReader(sr) { CloseInput = false })
-				{
-					try
-					{
-						return ser.Deserialize<JToken>(jtr);
-					}
-					catch (Exception ex)
-					{
-						if (throwError)
-						{
-							throw ex;
-						}
-						return null;
-					}
-				}
-			}
-		}
-
-		public static JArray DeserializeArray(Stream stream, Encoding encoding)
-		{
-			JArray result = new JArray();
-			JsonSerializer ser = JsonSerializer.CreateDefault();
-			using (StreamReader sr = new StreamReader(stream, encoding))
-			{
-				using (JsonTextReader jtr = new JsonTextReader(sr) { CloseInput = false, SupportMultipleContent = true })
-				{
-					while (jtr.Read())
-					{
-						try
-						{
-							JToken item = ser.Deserialize<JToken>(jtr);
-							result.Add(item);
-						}
-						catch
-						{
-							continue;
-						}
-					}
-				}
 			}
 			return result;
 		}
